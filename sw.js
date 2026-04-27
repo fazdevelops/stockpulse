5465494251f8e2d5234be958dc0a4b84547c3ec9
fazdevelops <faz.develops@gmail.com>
2026-04-27 23:03:43 +0400

    Updated sw.js file

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Clear all caches and unregister - force fresh load every time
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
+});
+// No caching - always fetch fresh
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
